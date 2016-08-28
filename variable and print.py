d173a3247b732ce1137ff6fcec050ae731f8bd6c
pilot118 <frank.hongming.zhang@gmail.com>
2016-08-28 21:43:02 +0800

    first upload

---
+++ variable and print.py
@@ -0,0 +1,24 @@
+#_*_coding:utf-8_*_
+
+my_name='zed A.shaw'
+my_age=32#not a lie
+my_height=74#inches
+my_weight=180#lbs
+my_eyes='Black'
+my_teeth='white'
+my_hair='Black'
+
+print('Let\'s talk about %s.'% my_name)
+print('He\'s %d inches tall.'%my_height)
+print('He\'s %d pounds heavy.'%my_weight)
+print('Actually that \'s not too heavy.')
+print('He\'s got %s eyes and %s hair.'%(my_eyes,my_hair))
+print('He\'s  teeth are usually %s depending on the coffee.'% my_teeth)
+
+#this line is tricky,try to get it exactly right
+print('if I add %d,%d,and %d Iget %d.'% (my_age,my_height,my_weight,my_age+my_height+my_weight))
+
+      
+
+
+
